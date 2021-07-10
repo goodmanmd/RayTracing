c46cd6a843d223adca7588883d92ae3c49efbbe9
Dean Goodman <goodmanmd@gmail.com>
2021-07-10 14:13:41 -0500

    Add a Scene to hold multiple objects

---
+++ InAWeekend/Geometry/Scene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace InAWeekend.Geometry
+{
+    class Scene : IHittable
+    {
+        private readonly List<IHittable> _objects = new List<IHittable>();
+
+        public void Add(IHittable hittableObject) => _objects.Add(hittableObject);
+        public void Clear() => _objects.Clear();
+        
+        public bool Hit(Ray r, float min, float max, out HitRecord hit)
+        {
+            var closestHit = max;
+            var hitSomething = false;
+            hit = default;
+
+            foreach (var hittable in _objects)
+            {
+                if (hittable.Hit(r, min, max, out var hitRecord) && hitRecord.T < closestHit)
+                {
+                    closestHit = hitRecord.T;
+                    hit = hitRecord;
+                    hitSomething = true;
+                }
+            }
+
+            return hitSomething;
+        }
+    }
+}
