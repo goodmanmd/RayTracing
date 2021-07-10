3efee73d6530fba951bfc5dc618993c2a38049da
Dean Goodman <goodmanmd@gmail.com>
2021-07-10 12:07:35 -0500

    Create a FrameBuffer class to hold render results

---
+++ InAWeekend/Rendering/FrameBuffer.cs
@@ -0,0 +1,27 @@
+﻿using InAWeekend.Geometry;
+
+namespace InAWeekend.Rendering
+{
+    class FrameBuffer
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        private readonly Color3[] _buffer;
+
+        public FrameBuffer(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            _buffer = new Color3[width * height];
+        }
+
+
+        public Color3 this[int w, int h]
+        {
+            get => _buffer[w + Width * h];
+            set => _buffer[w + Width * h] = value;
+        }
+    }
+}
