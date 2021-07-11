e076eefb73693310df0969ae2fb9360c210d5102
Dean Goodman <goodmanmd@gmail.com>
2021-07-10 20:43:10 -0500

    Helper for clamping output values to a range

---
+++ InAWeekend/Util/MathUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InAWeekend.Util
+{
+    public static class MathUtil
+    {
+        public static int Clamp(int value, int min, int max)
+        {
+            return Math.Max(min, Math.Min(max, value));
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            return Math.Max(min, Math.Min(max, value));
+        }
+    }
+}
